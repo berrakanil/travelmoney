30b1263f5b6ca735375367e59e9ec03f3e5f42b8
berrakanil <berrakanil@users.noreply.github.com>
2015-09-07 01:37:43 +0100

    Update App.java
    removing hello world:)

--- src/main/java/com/berrakanil/travelmoney/App.java
+++ src/main/java/com/berrakanil/travelmoney/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         SpringApplication.run(App.class, args);      
     }
 }
